4a8652f7465eb69d677657426aeb7db8ad1a8de3
Luiz Filho <luiz.filho@mutantbr.com>
2019-09-18 23:43:48 -0300

    add initial config swagger openapi specification

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocumentat from '../swagger.json'
 
 const server: express.Application = express()
 
 server.get('/', (request, response) => {
     response.send({ message: 'Welcome to my challenge'})
 })
+
+server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocumentat))
 
 server.listen(4446, () => {
     console.log(`Server is running! Welcome!`)
