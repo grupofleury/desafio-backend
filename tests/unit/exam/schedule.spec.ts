2667e28bf3c0d13a616d92d37340fd6f2286cecc
Luiz Filho <luiz.filho@mutantbr.com>
2019-09-22 01:09:00 -0300

    added unit test schedule exam

---
+++ tests/unit/exam/schedule.spec.ts
@@ -0,0 +1,56 @@
+import request from 'request-promise-native'
+import DB from '../../../data/db'
+import { fullName, getCpf, getDate, getFutureDate, getExamsMock } from '../utils/dataGenerate'
+import ScheduleService from '../../../src/services/schedule'
+
+jest.mock('request-promise-native')
+const mockedAxios = request as jest.Mocked<typeof request>
+
+let customer = {
+    cpf: '',
+    name: '',
+    dateOfBirth: new Date()
+}
+
+let scheduleService = new ScheduleService()
+const examsMock = getExamsMock()
+
+describe('Scheduling exam', () => {
+
+    beforeEach( async () => {
+
+        DB.resetForUnitTest()
+
+        let customerData = {
+            name: fullName(),
+            cpf: getCpf(),
+            dateOfBirth: getDate()
+        }
+
+        let connection = DB.connection()
+        customer = connection.addCustomer(customerData).data
+        mockedAxios.get.mockResolvedValue(examsMock)
+    })
+    
+    it('should be scheduled an exam for a patient enrolled in the database', async () => {
+        const data = {
+            examId: (Math.floor(Math.random() * 3) + 1).toString(),
+            cpf: customer.cpf,
+            date: getFutureDate()
+        }
+        mockedAxios.get.mockResolvedValue(examsMock)
+        let examResult = await scheduleService.save(data)
+        expect(examResult).toEqual(data)
+    })
+
+    it('should not be scheduled an exam  for the wrong exam id', async () => {
+        const data = {
+            examId: '11',
+            cpf: customer.cpf,
+            date: getFutureDate()
+        }
+        mockedAxios.get.mockResolvedValue(examsMock)
+        let examResult = await scheduleService.save(data)
+        expect(examResult).toEqual(null)
+    })
+})
--- tests/unit/utils/dataGenerate.ts
+++ tests/unit/utils/dataGenerate.ts
@@ -6,8 +6,14 @@ const fullName = () => Faker.name.firstName + ' ' + Faker.name.lastName
 
 const getDate = () => Faker.date.between('1920-01-01', '2019-09-01')
 
+const getFutureDate = () => Faker.date.between(new Date(), '2020-12-31')
+
+const getExamsMock = () => '{"exams": [{"id":"1","name":"17 soro","value":35.60},{"id":"2","name":"Acidificação Urinária","value":84.90},{"id":"3","name":"Ácido Ascórbico, plasma","value":99.90}]}'
+
 export {
     getCpf,
     fullName,
-    getDate
+    getDate,
+    getFutureDate,
+    getExamsMock
 }
