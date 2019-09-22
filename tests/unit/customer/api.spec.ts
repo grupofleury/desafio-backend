ca277f822569eef1a26477ed971d8fdd72d6969c
Luiz Filho <luiz.filho@mutantbr.com>
2019-09-22 20:43:41 -0300

    updated unit test sintaxe

--- tests/unit/customer/api.spec.ts
+++ tests/unit/customer/api.spec.ts
@@ -4,17 +4,19 @@ import ExamProvider from '../../../src/providers/exams'
 jest.mock('request-promise-native')
 const mockedAxios = request as jest.Mocked<typeof request>
 
-test('should return exam list', async () => {
-    const exams = '{"exams": [{"id":"1","name":"17 soro","value":35.60},{"id":"2","name":"Acidificação Urinária","value":84.90},{"id":"3","name":"Ácido Ascórbico, plasma","value":99.90}]}'
+describe('mock api', () => {
+    it('should return exam list', async () => {
+        const exams = '{"exams": [{"id":"1","name":"17 soro","value":35.60},{"id":"2","name":"Acidificação Urinária","value":84.90},{"id":"3","name":"Ácido Ascórbico, plasma","value":99.90}]}'
+        
+        const expected = [
+            { id: "1", name: '17 soro' },
+            { id: "2", name: 'Acidificação Urinária' },
+            { id: "3", name: 'Ácido Ascórbico, plasma' }
+        ]
     
-    const expected = [
-        { id: "1", name: '17 soro' },
-        { id: "2", name: 'Acidificação Urinária' },
-        { id: "3", name: 'Ácido Ascórbico, plasma' }
-    ]
-
-    mockedAxios.get.mockResolvedValue(exams)
-    let response = await ExamProvider.list()
-    expect(response).toEqual(expected)
-})
+        mockedAxios.get.mockResolvedValue(exams)
+        let response = await ExamProvider.list()
+        expect(response).toEqual(expected)
+    })
+})    
 
